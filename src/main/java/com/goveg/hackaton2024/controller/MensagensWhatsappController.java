a73f32381aae3cf8572cb0aee959dbe184035659
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2024-02-08 18:03:13 -0300

    adicionando endpoint de receber e enviar mensagens no whatsapp

---
+++ src/main/java/com/goveg/hackaton2024/controller/MensagensWhatsappController.java
@@ -0,0 +1,26 @@
+package com.goveg.hackaton2024.controller;
+
+import com.goveg.hackaton2024.integracao.GerenciadorWhatsapp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/whatsapp")
+public class MensagensWhatsappController {
+
+    private final GerenciadorWhatsapp gerenciadorWhatsapp;
+
+    @PostMapping("/enviar")
+    @ResponseStatus(HttpStatus.OK)
+    public void enviarMensagem() {
+        gerenciadorWhatsapp.enviarMensagem("mensagem enviada pelo controller");
+    }
+
+    @PostMapping("/receber")
+    @ResponseStatus(HttpStatus.OK)
+    public String receberMensagem(@RequestBody String body) {
+        return gerenciadorWhatsapp.receberMensagem(body);
+    }
+}
---
+++ src/main/java/com/goveg/hackaton2024/integracao/GerenciadorWhatsapp.java
@@ -0,0 +1,55 @@
+package com.goveg.hackaton2024.integracao;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class GerenciadorWhatsapp {
+
+    public String ACCOUNT_SID = System.getenv("ACCOUNT_SID");
+    public String AUTH_TOKEN = System.getenv("AUTH_TOKEN");
+
+
+    public void enviarMensagem(String mensagem) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("whatsapp:+554598281323"),
+                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                mensagem).create();
+
+        System.out.println(message.getSid());
+    }
+
+    public String receberMensagem(String mensagem) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        System.out.println("------------------------------------" + mensagem + "------------------------------------");
+        final Map<String, String> params = extractParams(mensagem);
+
+        final String bodyValue = params.get("Body");
+
+        System.out.println("------------------------------------ Valor do Body: " + bodyValue + "------------------------------------");
+        return bodyValue;
+    }
+
+    private static Map<String, String> extractParams(String url) {
+        Map<String, String> params = new HashMap<>();
+        String[] urlParts = url.split("&");
+        for (String part : urlParts) {
+            String[] keyValue = part.split("=");
+            if (keyValue.length == 2) {
+                String decodedValue = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
+                params.put(keyValue[0], decodedValue);
+            }
+        }
+        return params;
+    }
+
+}
